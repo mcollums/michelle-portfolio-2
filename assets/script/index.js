e7bdc5f8220f126a2e01b8560ceacca53b3d5d7c
mcollums <collumsmichelle@gmail.com>
2020-04-11 15:10:42 -0500

    click handlers started, modal showing

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
-    console.log("Linked");
+
+
+    const handleYes = () => {
+        console.log("handleYes");
+    }
+    
+    const handleNo = () => {
+        console.log("handleNo");
+    }
+
+//when user clicks yes or no button, call handleMatch
+//If no button, change card to new values
+    //Switch image, Name, Title, etc.
+//if yes, show "It's a match alert"
+    //take user to portfolio page
+
+
+$(".yes").on("click", handleYes);
+$(".no").on("click", handleNo);
 
 });
-
